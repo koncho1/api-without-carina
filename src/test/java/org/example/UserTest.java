aa35111355e5f236fdeb8063a7f4caf64fb6c070
koncho1 <xvx2402@gmail.com>
2025-05-14 18:12:13 +0200

    added validation against schema

--- src/test/java/org/example/UserTest.java
+++ src/test/java/org/example/UserTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import com.networknt.schema.ValidationMessage;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.*;
@@ -9,8 +11,11 @@ import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpHeaders;
 import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.message.BasicHeader;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -24,21 +29,24 @@ import java.util.List;
 //TODO:
 //add payload validation against json schema
 
-public class UserTest {
+public class UserTest extends BaseTest {
     @Test
     public void getUsers() throws IOException {
         HttpClient client = HttpClients.createDefault();
         HttpUriRequest request = new HttpGet("https://gorest.co.in/public/v2/users");
         String response = client.execute(request, new BasicHttpClientResponseHandler());
-        System.out.println(response);
+        List<ValidationMessage> errors = validatePayload(response, "getUsersTemplate");
+        Assert.assertTrue(errors.isEmpty());
 
     }
 
     @Test
     public void getUser() throws IOException {
+        HttpClient client = HttpClients.createDefault();
         HttpUriRequest request = new HttpGet("https://gorest.co.in/public/v2/users/7881661");
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        Assert.assertEquals(httpResponse.getCode(), 200);
+        String response = client.execute(request, new BasicHttpClientResponseHandler());
+        List<ValidationMessage> errors = validatePayload(response, "getUserTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
@@ -50,28 +58,33 @@ public class UserTest {
 
     @Test
     void postUser() throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://gorest.co.in/public/v2/users?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(String.format("https://gorest.co.in/public/v2/users"));
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("name", "John Doe"));
-        params.add(new BasicNameValuePair("email", "maisli@gmail.com"));
+        params.add(new BasicNameValuePair("email", String.format("%s@gmail.com", RandomStringUtils.random(10, true, true))));
         params.add(new BasicNameValuePair("gender", "male"));
         params.add(new BasicNameValuePair("status", "active"));
         httpPost.setEntity(new UrlEncodedFormEntity(params));
-        HttpResponse response = httpclient.execute(httpPost);
-        Assert.assertEquals(response.getCode(), 201);
+        Header header = new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        httpPost.setHeader(header);
+        String response = client.execute(httpPost, new BasicHttpClientResponseHandler());
+        List<ValidationMessage> errors = validatePayload(response, "postUserTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
     void postUserWithIncorrectParams() throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://gorest.co.in/public/v2/users?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        HttpPost httpPost = new HttpPost(String.format("https://gorest.co.in/public/v2/users"));
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("name", "John Doe"));
-        params.add(new BasicNameValuePair("mail", "maili@gmail.com"));
+        params.add(new BasicNameValuePair("mail", String.format("%s@gmail.com", RandomStringUtils.random(10, true, true))));
         params.add(new BasicNameValuePair("gender", "male"));
         params.add(new BasicNameValuePair("status", "active"));
         httpPost.setEntity(new UrlEncodedFormEntity(params));
+        Header header = new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        httpPost.setHeader(header);
         HttpResponse response = httpclient.execute(httpPost);
         Assert.assertEquals(response.getCode(), 422);
     }
@@ -79,49 +92,60 @@ public class UserTest {
     @Test
     void postUserWithIncorrectMailFormat() throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("https://gorest.co.in/public/v2/users?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        HttpPost httpPost = new HttpPost(String.format("https://gorest.co.in/public/v2/users?access-token=%s", token));
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("name", "Mary"));
         params.add(new BasicNameValuePair("mail", "marygmail.com"));
         params.add(new BasicNameValuePair("gender", "female"));
         params.add(new BasicNameValuePair("status", "active"));
         httpPost.setEntity(new UrlEncodedFormEntity(params));
+        Header header = new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        httpPost.setHeader(header);
         HttpResponse response = httpclient.execute(httpPost);
         Assert.assertEquals(response.getCode(), 422);
     }
 
     @Test
     public void patchUser() throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPatch httpPut = new HttpPatch("https://gorest.co.in/public/v2/users/7881661?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPatch httpPatch = new HttpPatch(String.format("https://gorest.co.in/public/v2/users/7887693?access-token=%s", token));
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("name", "John Doe"));
-        params.add(new BasicNameValuePair("email", "mail@gmail.example"));
+        params.add(new BasicNameValuePair("email", String.format("%s@gmail.com", RandomStringUtils.random(10, true, true))));
         params.add(new BasicNameValuePair("gender", "male"));
         params.add(new BasicNameValuePair("status", "active"));
-        httpPut.setEntity(new UrlEncodedFormEntity(params));
-        HttpResponse response = httpclient.execute(httpPut);
-        Assert.assertEquals(response.getCode(), 200);
+        httpPatch.setEntity(new UrlEncodedFormEntity(params));
+        Header header = new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        httpPatch.setHeader(header);
+        String response = client.execute(httpPatch, new BasicHttpClientResponseHandler());
+        List<ValidationMessage> errors = validatePayload(response, "patchUserTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
     public void putUser() throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPut httpPut = new HttpPut("https://gorest.co.in/public/v2/users/7824134?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPut httpPut = new HttpPut(String.format("https://gorest.co.in/public/v2/users/7887693?access-token=%s", token));
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("name", "John Doe"));
-        params.add(new BasicNameValuePair("email", "mail@gmail.example"));
+        params.add(new BasicNameValuePair("email", String.format("%s@gmail.com", RandomStringUtils.random(10, true, true))));
         params.add(new BasicNameValuePair("gender", "male"));
         params.add(new BasicNameValuePair("status", "active"));
         httpPut.setEntity(new UrlEncodedFormEntity(params));
-        HttpResponse response = httpclient.execute(httpPut);
-        Assert.assertEquals(response.getCode(), 200);
+        Header header = new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        httpPut.setHeader(header);
+        String response = client.execute(httpPut, new BasicHttpClientResponseHandler());
+        List<ValidationMessage> errors = validatePayload(response, "putUserTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
     public void deleteUser() throws IOException {
-        HttpUriRequest request = new HttpDelete("https://gorest.co.in/public/v2/users/7887706?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        HttpClient client = HttpClients.createDefault();
+        HttpUriRequest request = new HttpDelete(String.format("https://gorest.co.in/public/v2/users/7887693?access-token=%s", token));
+        Header header = new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        request.setHeader(header);
+        HttpResponse httpResponse = client.execute(request);
         Assert.assertEquals(httpResponse.getCode(), 204);
     }
 
